c45a58060d2dcd4e9469fdb143fffb309cca6fea
abukawsar2021 <77399411+abukawsar2021@users.noreply.github.com>
2021-03-31 15:03:24 +0600

    Delete flickr-gallery-shortcode.php

--- flickr-gallery-shortcode.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-//flickr feed shortcode
-function santo_flickr_feed_shortcode() {
-
-	return '<ul id="flickr-gallery" class="flickr-gallery flickr-feed clearfix"></ul>' ;
-}
-add_shortcode("santo_flickr_feed","santo_flickr_feed_shortcode");
